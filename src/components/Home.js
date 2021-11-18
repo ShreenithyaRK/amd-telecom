6844c7b4927fa19fa9a2dd06d338e0c63d2d2fff
ShreenithyaRK <qtnithya@gmail.com>
2021-11-18 15:59:24 +0530

    minor fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,7 +24,6 @@ export default function Home() {
          }
  }).then(res => {
      setLoading(false);
-     setSearchQuery(searchInput)
      setSearchResult(res.data.organic_results);
     
  })
