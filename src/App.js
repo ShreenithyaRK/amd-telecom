d4181b4d0500c044e392c8d2db44a46da75399b2
ShreenithyaRK <qtnithya@gmail.com>
2021-11-18 16:12:20 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   
   return (
-    <Router>
+    <Router  basename={process.env.PUBLIC_URL}>
       <div className="App">
         <div className="navbar">
           <NavLink activeClassName="active" to="/">
